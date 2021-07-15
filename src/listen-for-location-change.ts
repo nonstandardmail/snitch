222e5106ae4837b6bc1934e49907f51bbee2448d
Boris Chumichev <boris362@yandex.ru>
2021-07-15 12:57:37 +0300

    Add location change listener

---
+++ src/listen-for-location-change.ts
@@ -0,0 +1,27 @@
+const patchMethodWithCallbackCall = (methodName: Function, context: any, callback: Function) => (
+  ...args: any
+) => {
+  const result = methodName.apply(context, [...args])
+  setTimeout(() => callback(), 0)
+  return result
+}
+
+const listeners: Function[] = []
+
+const onStateChange = () => listeners.forEach(listener => listener())
+
+window.history.pushState = patchMethodWithCallbackCall(
+  window.history.pushState,
+  window.history,
+  onStateChange
+)
+window.history.replaceState = patchMethodWithCallbackCall(
+  window.history.replaceState,
+  window.history,
+  onStateChange
+)
+window.addEventListener('popstate', onStateChange)
+
+export default function listenForLocationChange(onLocationChange: Function) {
+  listeners.push(onLocationChange)
+}

3708d2076b429a2f28c53173fcb2a8f1514add5b
Boris Chumichev <boris362@yandex.ru>
2021-08-05 20:28:59 +0300

    Fix ERROR_NO_VK_BRIDGE error name

--- packages/snitch-plugin-vkbridge-transport/vkbridge-transport.ts
+++ packages/snitch-plugin-vkbridge-transport/vkbridge-transport.ts
@@ -2,7 +2,7 @@ import { EventTransport } from '../common/plugin-interfaces'
 import { TrackerEventPayload } from '../common/tracker-interfaces'
 import { VKBridge } from './vkbridge'
 
-export const ERROR_NO_VK_BRIDGE = 'initErrorNoTMRCounterId'
+export const ERROR_NO_VK_BRIDGE = 'initErrorNoVKBridge'
 
 export default function vkbridgeTransportPlugin(bridge: VKBridge): EventTransport {
   if (!bridge) throw TypeError(ERROR_NO_VK_BRIDGE)

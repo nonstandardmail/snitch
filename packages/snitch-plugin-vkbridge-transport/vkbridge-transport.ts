e2b865b8f2a690347c8ac3a376acef245df3354d
Boris Chumichev <boris362@yandex.ru>
2021-11-29 13:28:37 +0300

    Fix iOS native mini-apps custom events by converting all event params to strings

--- packages/snitch-plugin-vkbridge-transport/vkbridge-transport.ts
+++ packages/snitch-plugin-vkbridge-transport/vkbridge-transport.ts
@@ -7,6 +7,17 @@ export const ERROR_NO_VK_BRIDGE = 'initErrorNoVKBridge'
 export default function vkbridgeTransportPlugin(): EventTransport {
   return {
     sendEvent(eventName: string, eventParams?: TrackerEventPayload) {
+      /*
+        For some reason VKBridge for iOS native clients
+        does not send events with numeric values in params
+        so we have to convert all no-nstring params to strings
+      */
+      if (eventParams) {
+        for (const paramKey of Object.keys(eventParams)) {
+          if (typeof eventParams[paramKey] !== 'string')
+            eventParams[paramKey] = eventParams[paramKey].toString()
+        }
+      }
       vkBridgeSend({
         event_name: eventName,
         event_params: eventParams

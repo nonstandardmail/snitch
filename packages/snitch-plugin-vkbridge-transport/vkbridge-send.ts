692f3d17f108a96e651f7483d2be4d12bf29674d
Boris Chumichev <b.chumichev@corp.mail.ru>
2024-05-30 12:02:15 +0300

    ReactNative Webview support for mini-apps

--- packages/snitch-plugin-vkbridge-transport/vkbridge-send.ts
+++ packages/snitch-plugin-vkbridge-transport/vkbridge-send.ts
@@ -1,16 +1,34 @@
+/** This one implements vk bridge send method so that
+ * bridge does not have to be loaded prior to sending events */
+
 const method = 'VKWebAppTrackEvent'
 const webFrameId = undefined
-const version = '2.5.0'
+const version = '2.14.1'
 
+/** Is the client side runtime environment */
 export const IS_CLIENT_SIDE = typeof window !== 'undefined'
+
+/** Is the runtime environment an Android app */
 export const IS_ANDROID_WEBVIEW = Boolean(IS_CLIENT_SIDE && (window as any).AndroidBridge)
+
+/** Is the runtime environment an iOS app */
 export const IS_IOS_WEBVIEW = Boolean(
   IS_CLIENT_SIDE &&
     (window as any).webkit &&
     (window as any).webkit.messageHandlers &&
     (window as any).webkit.messageHandlers.VKWebAppClose
 )
+
+export const IS_REACT_NATIVE_WEBVIEW = Boolean(
+  IS_CLIENT_SIDE &&
+    (window as any).ReactNativeWebView &&
+    typeof (window as any).ReactNativeWebView.postMessage === 'function'
+)
+
+/** Is the runtime environment a browser */
 export const IS_WEB = IS_CLIENT_SIDE && !IS_ANDROID_WEBVIEW && !IS_IOS_WEBVIEW
+
+/** Android VK Bridge interface. */
 const androidBridge: Record<string, (serializedData: string) => void> | undefined = IS_CLIENT_SIDE
   ? (window as any).AndroidBridge
   : undefined
@@ -18,6 +36,11 @@ const androidBridge: Record<string, (serializedData: string) => void> | undefine
 /** iOS VK Bridge interface. */
 const iosBridge: Record<string, { postMessage?: (data: any) => void }> | undefined = IS_IOS_WEBVIEW
   ? (window as any).webkit.messageHandlers
+  : undefined
+
+/** Web VK Bridge interface. */
+const webBridge: { postMessage?: (message: any, targetOrigin: string) => void } | undefined = IS_WEB
+  ? parent
   : undefined
 
 interface VKWebAppTrackEventPayload {
@@ -39,9 +62,19 @@ export default function send(props: VKWebAppTrackEventPayload) {
     iosBridge[method].postMessage!(props)
   }
 
+  // Sending data through React Native bridge
+  else if (IS_REACT_NATIVE_WEBVIEW) {
+    ;(window as any).ReactNativeWebView.postMessage(
+      JSON.stringify({
+        handler: method,
+        params: props
+      })
+    )
+  }
+
   // Sending data through web bridge
-  else if (IS_WEB) {
-    parent.postMessage(
+  else if (webBridge && typeof webBridge.postMessage === 'function') {
+    webBridge.postMessage(
       {
         handler: method,
         params: props,
